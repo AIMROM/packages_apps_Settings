ad40acef2a2cdb98cbe7b096e6dcb9eada8ea2f5
maxwen <max.weninger@gmail.com>
2017-05-27 18:21:52 +0200

    Settings: enable fast scroll for all app list views
    Why? - Why not?
    
    Change-Id: Id67fa3e331aa7986f4a83eaaee39dc782a28e020
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- src/com/android/settings/applications/ManageApplications.java
+++ src/com/android/settings/applications/ManageApplications.java
@@ -513,7 +513,7 @@ public class ManageApplications extends InstrumentedPreferenceFragment
             case LIST_TYPE_PHOTOGRAPHY:
                 return mSortOrder == R.id.sort_order_alpha;
             default:
-                return false;
+                return true;
         }
     }
 
@@ -1191,7 +1191,7 @@ public class ManageApplications extends InstrumentedPreferenceFragment
         }
 
         private void rebuildSections() {
-            if (mEntries != null && mManageApplications.mListView.isFastScrollEnabled()) {
+            if (mEntries != null && mManageApplications.isFastScrollEnabled()) {
                 // Rebuild sections
                 if (mIndex == null) {
                     LocaleList locales = mContext.getResources().getConfiguration().getLocales();
