7eb2a9c9b96762c7d59b67cf2236af84cf55589e
Anand Ramachandran <anand.nv1998@gmail.com>
2017-09-20 19:13:22 +0530

    Device info screen fix settings forceclose and not triggering platlogo
    *fix whole settings fc -_-
    *fix plat logo not triggering
    
    Signed-off-by: Anand Ramachandran <anand.nv1998@gmail.com>

--- src/com/android/settings/deviceinfo/AimVersionPreferenceController.java
+++ src/com/android/settings/deviceinfo/AimVersionPreferenceController.java
@@ -16,20 +16,42 @@
 package com.android.settings.deviceinfo;
 
 import android.content.Context;
-import android.os.SystemProperties;
+import android.content.Intent;
+import android.os.Build;
+import android.os.SystemClock;
+import android.os.UserHandle;
+import android.os.UserManager;
 import android.support.v7.preference.Preference;
+import android.support.v7.preference.PreferenceScreen;
+import android.text.TextUtils;
+import android.util.Log;
 
 import com.android.settings.core.PreferenceController;
-import com.android.settings.R;
-import com.android.settings.Utils;
+import com.android.settings.core.lifecycle.Lifecycle;
+import com.android.settings.core.lifecycle.LifecycleObserver;
+import com.android.settings.core.lifecycle.events.OnResume;
+import com.android.settingslib.RestrictedLockUtils;
 
-public class AimVersionPreferenceController extends PreferenceController {
 
-    private static final String AIM_VERSION = "aim_version";
-    private static final String AIM_PROP = "ro.aim.version";
+public class AimVersionPreferenceController extends PreferenceController
+        implements LifecycleObserver, OnResume {
 
-    public AimVersionPreferenceController(Context context) {
+    private static final String TAG = "AimVersionPref";
+    private static final String KEY_AIM_VERSION = "aim_version";
+
+    private final UserManager mUserManager;
+
+    private RestrictedLockUtils.EnforcedAdmin mFunDisallowedAdmin;
+    private boolean mFunDisallowedBySystem;
+
+    private long[] mHits = new long[3];
+
+    public AimVersionPreferenceController(Context context, Lifecycle lifecycle) {
         super(context);
+        mUserManager = (UserManager) context.getSystemService(Context.USER_SERVICE);
+        if (lifecycle != null) {
+            lifecycle.addObserver(this);
+        }
     }
 
     @Override
@@ -38,14 +60,57 @@ public class AimVersionPreferenceController extends PreferenceController {
     }
 
     @Override
-    public String getPreferenceKey() {
-        return AIM_VERSION;
+    public void displayPreference(PreferenceScreen screen) {
+        super.displayPreference(screen);
+        final Preference pref = screen.findPreference(KEY_AIM_VERSION);
+        if (pref != null) {
+            pref.setSummary(android.os.Build.AIM_DISPLAY_VERSION);
+            pref.setEnabled(true);
+        }
     }
 
     @Override
-    public void updateState(Preference preference) {
-        super.updateState(preference);
-        preference.setSummary(SystemProperties.get(AIM_PROP,
-                mContext.getResources().getString(R.string.device_info_default)));
+    public String getPreferenceKey() {
+        return KEY_AIM_VERSION;
+    }
+
+    @Override
+    public void onResume() {
+        mFunDisallowedAdmin = RestrictedLockUtils.checkIfRestrictionEnforced(
+                mContext, UserManager.DISALLOW_FUN, UserHandle.myUserId());
+        mFunDisallowedBySystem = RestrictedLockUtils.hasBaseUserRestriction(
+                mContext, UserManager.DISALLOW_FUN, UserHandle.myUserId());
+    }
+
+    @Override
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (!TextUtils.equals(preference.getKey(), KEY_AIM_VERSION)) {
+            return false;
+        }
+        System.arraycopy(mHits, 1, mHits, 0, mHits.length - 1);
+        mHits[mHits.length - 1] = SystemClock.uptimeMillis();
+        if (mHits[0] >= (SystemClock.uptimeMillis() - 500)) {
+            if (mUserManager.hasUserRestriction(UserManager.DISALLOW_FUN)) {
+                if (mFunDisallowedAdmin != null && !mFunDisallowedBySystem) {
+                    RestrictedLockUtils.sendShowAdminSupportDetailsIntent(mContext,
+                            mFunDisallowedAdmin);
+                }
+                Log.d(TAG, "Sorry, no fun for you!");
+                return false;
+            }
+
+            final Intent intent = new Intent(Intent.ACTION_MAIN)
+                    .putExtra("is_aim", true)
+                    .setClassName(
+                            "android", com.android.internal.app.PlatLogoActivity.class.getName());
+            try {
+                mContext.startActivity(intent);
+                return true;
+            } catch (Exception e) {
+                Log.e(TAG, "Unable to start activity " + intent.toString());
+            }
+        }
+        return false;
     }
 }
+
